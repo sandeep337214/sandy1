77d11ca8a75a6df149f1a78402b38baef0b86f60
sandeep337214 <157687217+sandeep337214@users.noreply.github.com>
2024-06-07 20:58:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-      <Route exact path="/
-      " element={<LandingPage />} />
+      <Route exact path="/" element={<LandingPage />} />
         <Route exact path="/signin" element={<SignIn />} />
         <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="/newlandingpage" element={<NewLandingPage />} />
